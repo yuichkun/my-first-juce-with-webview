02ac9fe6042ced1f4e2d7ad84deebfb2801657cf
yuichkun <yuichkun@gmail.com>
2024-10-26 02:02:26 +0900

    enable WebView

--- plugin/include/JuceWebViewPlugin/PluginEditor.h
+++ plugin/include/JuceWebViewPlugin/PluginEditor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "JuceWebViewPlugin/PluginProcessor.h"
+#include <juce_gui_extra/juce_gui_extra.h>
 
 //==============================================================================
 namespace webview_plugin {
@@ -11,13 +12,14 @@ public:
     ~AudioPluginAudioProcessorEditor() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
     void resized() override;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     AudioPluginAudioProcessor& processorRef;
+
+    juce::WebBrowserComponent webView;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
 };
--- plugin/src/PluginEditor.cpp
+++ plugin/src/PluginEditor.cpp
@@ -3,10 +3,14 @@
 
 //==============================================================================
 namespace webview_plugin {
-AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
-    : AudioProcessorEditor (&p), processorRef (p)
+AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(
+    AudioPluginAudioProcessor &p)
+    : AudioProcessorEditor(&p), processorRef(p),
+    webView(juce::WebBrowserComponent::Options{})
 {
     juce::ignoreUnused (processorRef);
+
+    addAndMakeVisible(webView);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
@@ -17,19 +21,9 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
 }
 
 //==============================================================================
-void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
-{
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
-}
 
 void AudioPluginAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    webView.setBounds(getLocalBounds());
 }
 }
